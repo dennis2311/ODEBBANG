9a9704cfb76b06ef9f49a96c03f511c14a054393
JieunKim <78676660+mellykim123@users.noreply.github.com>
2023-06-05 23:57:20 +0900

    add: css class , text and images

--- src/Result/Result.jsx
+++ src/Result/Result.jsx
@@ -1,5 +1,7 @@
 import "./result.css";
-import YonseiResult from "../Resource/yonsei.png"
+import YonseiResult from "../Resource/yonsei.png";
+import Soccer from "../Resource/soccer.png";
+import Lightening from "../Resource/lightening.png"
 /**
  * 이벤트 결과 페이지입니다.
  * 이벤트 결과를 확인하고 (총 참여자 수) 카카오톡 공유하기를 할 수 있습니다.
@@ -10,16 +12,18 @@ export function Result({ univ, selectedUniv }) {
   return (
     <div className="result container">
       <div className="result__title">
-        <img src={YonseiResult} alt="yonsei" />
+        <div><img src={YonseiResult} alt="yonsei" /></div>
         <div>투표현황</div>
-        <img src={YonseiResult} alt="yonsei" />
+        <div><img src={YonseiResult} alt="yonsei" /></div>
       </div>
       <div className="result_totalCnt">
         <span className="result_yonseiCnt">11,220</span>
+        <span className="result_versus"><img src={Lightening} /></span>
         <span className="result_koreaCnt">11,220</span>
       </div>
       <span className="result__content">{`어차피 우승은 ${univ}!`}</span>
-
+      <img src={Soccer} alt="soccer" />
+      <div>공유해서 우리 학교 응원하기</div>
       <div className="result__shareBtn">{`친구에게 ${selectedUniv} 응원시키기`}</div>
     </div>
   );
