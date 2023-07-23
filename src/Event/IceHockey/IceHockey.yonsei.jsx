8a936c68363ccd53d3ebb279fc00e3fe9414b0b3
Jerry <blay210@yonsei.ac.kr>
2023-07-23 19:51:34 +0900

    지우는거 테스트1

--- src/Event/IceHockey/IceHockey.yonsei.jsx
+++ src/Event/IceHockey/IceHockey.yonsei.jsx
@@ -2,18 +2,26 @@
  * 종목 - 아이스하키 응원 화면 (연세대학교 강제 응원)
  * @author 현웅
  */
+import React, { useState } from "react";
+
+
 export function IceHockeyYonsei({ goNextEvent }) {
-  console.log("2023/05/29 문범용");
+  const [isVisible, set_isVisible] = useState(true);
+  const handleVisibility = ()=> {
+    set_isVisible(!isVisible);
+  };
+
+
   return (
     <>
       <h1 className="event__title">아이스하키</h1>
 
       <div className="event__btnRow">
-        <button id="korea">고대</button>
+        <button id="korea" onClick={handleVisibility}>고대</button>
         <button id="yonsei" onClick={goNextEvent}>
           연대
         </button>
       </div>
     </>
   );
-}
+};
