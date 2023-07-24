6f28ef13a84c16e0c895a83cf3042c1823ce8923
she1018 <she020108@gmail.com>
2023-07-24 15:20:05 +0900

    Baseball yonsei 기본 UI 구현 (효은)

--- src/Event/Baseball/Baseball.yonsei.jsx
+++ src/Event/Baseball/Baseball.yonsei.jsx
@@ -2,18 +2,54 @@
  * 종목 - 야구 응원 화면 (연세대학교 강제 응원)
  * @author 현웅
  */
-export function BaseballYonsei({ goNextEvent }) {
-  console.log("고려대인 유한 수정함");
-  return (
-    <>
-      <h1 className="event__title">야구</h1>
 
-      <div className="event__btnRow">
-        <button id="korea">고대</button>
-        <button id="yonsei" onClick={goNextEvent}>
-          연대
-        </button>
+import "./Baseball.yonsei.css";
+import anime from 'animejs';
+
+export function BaseballYonsei({ goNextEvent }) {
+
+  const handleKoreaClick = () => {
+    console.log("고대 클릭");
+  };
+
+  const handleYonseiClick = () => {
+    anime({
+      targets: '.page-wrapper',
+      backgroundPosition: '150% 300%',
+      duration: 1500,
+      easing: 'easeOutExpo',
+      complete: () => {
+        setTimeout(() => {
+          goNextEvent();
+        }, 3500);
+      },
+    });
+  };
+
+  return (
+    <div className="page-wrapper">
+      <div className="header-container">
+        <h5 className="headertext-round">Round 2</h5>
+        <h3 className="headertext-event">야구</h3>
+        <div className="prompt-container">
+          <h1 className="prompt-text">이길 것 같은 팀을</h1>
+          <h1 className="prompt-text">선택해주세요</h1>
+        </div>
       </div>
-    </>
+      <div className="body-container"></div>
+      <div className="buttons-container">
+        <div className="button-container">
+          <div id="korea" className="univ-button" onClick={handleKoreaClick}>
+            <img src="images/korea_logo.svg" alt="고대" />
+          </div>
+        </div>
+        <img className="versus-icon" src="images/lightning.png" alt="아이콘" />
+        <div className="button-container">
+          <div id="yonsei" className="univ-button" onClick={handleYonseiClick}>
+            <img src="images/yonsei_logo.svg" alt="연대" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
