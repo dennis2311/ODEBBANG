d9f1a3a8268ac5a3e818970e6629f3363ee237a1
jinny <rlagudwls0@korea.ac.kr>
2023-07-26 20:24:31 +0900

    고려대 럭비 확대

--- src/Event/Rugby/Rugby.korea.jsx
+++ src/Event/Rugby/Rugby.korea.jsx
@@ -3,34 +3,50 @@
  * @author 현웅
  */
 
-import React, { useRef } from 'react';
-import anime from 'animejs';
+import { useRef, useState } from 'react';
 
 export function RugbyKorea({ goNextEvent }) {
-  console.log("형진 수정");
-  const buttonRef = useRef(null);
+  console.log('형진 수정');
+  const [isZoomed, setIsZoomed] = useState(false);
+  const [message, setMessage] = useState('이길 것 같은 팀은?');
 
-  const handleButtonClick = () => {
-    anime({
-      targets: buttonRef.current,
-      translateX: '100vw',
-      duration: 1000,
-      easing: 'easeOutExpo'
-    });
+
+  const handleZoomClick = () => {
+    setMessage(isZoomed);
+    setIsZoomed(!isZoomed);
+    
+    setTimeout(() => {
+      goNextEvent();
+    }, 2000); 
   };
 
+  
   return (
-    <>
+    
+    <div className={`event container ${isZoomed ? 'zoomed' : ''}`}>
+
+      <p className="message">
+        <span className="event__message">
+          이길 것 같{''}
+          {isZoomed && <span className="event__changed-message">지 않</span>}
+        </span>
+        <span className="event__message">
+          은 팀은?
+        </span>
+      </p>
       <h1 className="event__title">럭비</h1>
+
+      
 
       <div className="event__btnRow">
         <button id="korea" onClick={goNextEvent}>
           고대
         </button>
-        <button id="yonsei" onClick={handleButtonClick} ref={buttonRef}>
-          연대
+        <button id="yonsei" onClick={handleZoomClick}>
+          연대ㅎㅎ
         </button>
       </div>
-    </>
+    </div>
   );
 }
+
