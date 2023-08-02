b2bf66bb0b6fb13309d885ed572d2bae6fa6f2c2
Jerry <blay210@yonsei.ac.kr>
2023-08-02 20:28:20 +0900

    BasketBall-Yonsei(범용) flip 효과 완성

--- src/Event/Basketball/Basketball.yonsei.jsx
+++ src/Event/Basketball/Basketball.yonsei.jsx
@@ -4,9 +4,10 @@
  */
 
 import React, { useRef, useState } from "react";
+
 import './Basketball.yonsei.css';
 
-// export function IceHockeyYonsei({ goNextEvent }) {
+// export function BasketballYonsei({ goNextEvent }) {
 //   const [isVisible, set_isVisible] = useState(true);
 //   const handleVisibility = ()=> {
 //     set_isVisible(false);
@@ -14,7 +15,7 @@ import './Basketball.yonsei.css';
 
 //   return (
 //     <>
-//       <h1 className="event__title">아이스하키</h1>
+//       <h1 className="event__title">농구</h1>
 //       <div className="event__btnRow">
 //         <button style={{visibility: isVisible?"visible":"hidden"}} id="korea" onClick={handleVisibility}>고대</button>
 //         <button id="yonsei" onClick={goNextEvent}>
@@ -25,32 +26,88 @@ import './Basketball.yonsei.css';
 //   );
 // };
 
+///////////////////////////////////////////////////////////////////////////////////////////
 
-export function IceHockeyYonsei({ goNextEvent }) {
-  const [clickCount, set_clickCount] = useState(0);
-  const buttonRef = useRef();
-  const [isAnimating, set_isAnimating] = useState(false);
+// export function BasketballYonsei({ goNextEvent }) {
+//   const [clickCount, set_clickCount] = useState(0);
+//   const buttonRef = useRef();
+//   const [isAnimating, set_isAnimating] = useState(false);
 
-  const handleCount = () => {
-    set_isAnimating(true);
+//   const handleCount = () => {
+//     set_isAnimating(true);
 
-    setTimeout(() => {
-      // Reduce the button size by 10% on each click
-      const scale = 1 - 0.25 * (clickCount + 1);
-      buttonRef.current.style.transform = `scale(${scale})`;
-      set_isAnimating(false);
-      set_clickCount((prevClickCount) => prevClickCount + 1);
-    }, 200); // Duration of the animation in milliseconds (0.5 seconds in this example)
+//     setTimeout(() => {
+//       const scale = 1 - 0.25 * (clickCount + 1);
+//       buttonRef.current.style.transform = `scale(${scale})`;
+//       set_isAnimating(false);
+//       set_clickCount((prevClickCount) => prevClickCount + 1);
+//     }, 200);
+//   };
+
+//   return (
+//     <>
+//       <h1 className="event__title">농구</h1>
+//       <div className="event__btnRow">
+//         <button ref={buttonRef} id="korea1" className={isAnimating ? 'animating' : ''} onClick={handleCount}>고대</button>
+//         <button id="yonsei1" onClick={goNextEvent}>
+//           연대
+//         </button>
+//       </div>
+//     </>
+//   );
+// };
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// export function BasketballYonsei({ goNextEvent }) {
+//   const [KbtnClicked, setClickedStatus] = useState(false);
+//   const YbuttonRef = useRef();
+//   const KbuttonRef = useRef();
+
+//   const handleKoreabtn = () => {
+//     setClickedStatus(true);
+//   };
+
+//   return (
+//     <>
+//       <h1 className="event__title">농구</h1>
+//       <div className="event__btnRow">
+//         { KbtnClicked ?
+//           (<button ref = {YbuttonRef} id="yonsei" onClick={goNextEvent}>연대</button>) :
+//           (<button ref = {KbuttonRef} id="korea" onClick={handleKoreabtn}>고대</button>)
+//         }
+//         <button id="yonsei" onClick={goNextEvent}>
+//           연대
+//         </button>
+//       </div>
+//     </>
+//   );
+// };
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+export function BasketballYonsei({ goNextEvent }) {
+  const [isClicked, setClicked] = useState(false);
+
+  const handleKbtn = () => {
+    setClicked(true);
   };
 
   return (
     <>
-      <h1 className="event__title">아이스하키</h1>
+      <h1 className="event__title">농구</h1>
       <div className="event__btnRow">
-        <button ref={buttonRef} id="korea" className={isAnimating ? 'animating' : ''} onClick={handleCount}>고대</button>
-        <button id="yonsei" onClick={goNextEvent}>
-          연대
-        </button>
+        <div className="btn_container">
+          <button id="btn1" className={`${isClicked ? 'flipK' : ''}`} onClick={handleKbtn}>고대</button>
+          {isClicked && (<button id="btn2" className={`${isClicked ? 'flipY' : ''}`} onClick={goNextEvent}>연대</button>)}
+        </div>
+        <div className="btn_container">
+          <button id="yonsei" onClick={goNextEvent}>
+            연대
+          </button>
+        </div>
       </div>
     </>
   );
