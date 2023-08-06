43b2b4dba56eca99fdb850359e4333359e91314c
JieunKim <78676660+mellykim123@users.noreply.github.com>
2023-08-06 20:09:10 +0900

    css 수정

--- src/Result/Result.jsx
+++ src/Result/Result.jsx
@@ -3,10 +3,9 @@ import "./result.css";
 import YonseiResult from "../Resource/yonsei.png";
 import Soccer from "../Resource/soccer.png";
 import Lightening from "../Resource/lightening.png"
-import JSConfetti from "js-confetti";
 import { conteffi } from "../App/App";
 import { useState } from "react";
-
+// import { useMediaQuery } from 'react-responsive'
 /**
  * 이벤트 결과 페이지입니다.
  * 이벤트 결과를 확인하고 (총 참여자 수) 카카오톡 공유하기를 할 수 있습니다.
@@ -14,6 +13,16 @@ import { useState } from "react";
  * @author 현웅
  */
 export function Result({ univ, selectedUniv }) {
+  /* 화면크기 */
+  // const isDesktop = useMediaQuery({
+  //   query: '(max-width: 1224px)'
+  // })
+
+  // const isMobile = useMediaQuery({
+  //   query: '(max-width: 375px)'
+  // })
+  const yonsei_participants = 11220;
+  const korea_participants = 11220;
 
   const addConfetti = () => {
     conteffi.addConfetti({
@@ -24,19 +33,19 @@ export function Result({ univ, selectedUniv }) {
   };
   useEffect(() => {
     addConfetti();
+    console.log(selectedUniv)
   }, []);
   return (
-
-    <div className="result container">
+    <div className={`result container ${univ == "KOREA" ? 'korea' : ''}`}>
       <div className="result__title">
         <div><img src={YonseiResult} alt="yonsei" /></div>
         <div>투표현황</div>
         <div><img src={YonseiResult} alt="yonsei" /></div>
       </div>
       <div className="result_totalCnt">
-        <span className="result_yonseiCnt">11,220</span>
+        <span className="result_yonseiCnt">{yonsei_participants}</span>
         <span className="result_versus"><img src={Lightening} /></span>
-        <span className="result_koreaCnt">11,220</span>
+        <span className="result_koreaCnt">{korea_participants}</span>
       </div>
       <span className="result__content">{`어차피 우승은 ${univ}!`}</span>
       <img src={Soccer} alt="soccer" />
