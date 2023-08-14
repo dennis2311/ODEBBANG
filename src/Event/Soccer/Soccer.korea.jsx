19fa48134811e32ce927cdc543985f6752459270
hw1204 <hw971204@naver.com>
2023-08-14 14:02:31 +0900

    soccer korea 애니메이션 추가

--- src/Event/Soccer/Soccer.korea.jsx
+++ src/Event/Soccer/Soccer.korea.jsx
@@ -2,16 +2,48 @@ import { useState } from "react";
 import "./soccer.css";
 import { easeInOut, motion } from "framer-motion";
 
-
 export function SoccerKorea({ goNextEvent }) {
   const variants = {
     click: {
-      scale: [1, 1.2, 0.8, 1.1, 0.9, 1],
-      rotate: [0, 10, -10, 5, -5, 0],
+      scale: [1, 0.6],
+      rotate: [0, 120, 240, 360, 480],
+      y: [0, 30, 60, 90, 130],
+      x: [0, 400, 0, -60, -120],
+      borderRadius: [0, 5, 10, 15, 20],
+    },
+    scaled_K: {
+      scale: [1, 1.2],
+      y: [0, -30, 20],
+      x: [0, 0, -90],
+    },
+    scaled_Y: {
+      scale: [1, 0.8],
+      y: [0, 40],
+      opacity: [1, 1, 0],
+    },
+    ball: {
+      y: [0, 0, 20],
+      x: [0, 0, -100],
+    },
+    korea_logo: {
+      x: [0, 0, 110],
+    },
+    yonsei_logo: {
+      opacity: [1, 0],
+      scale: [1, 0.8],
+      y: [0, 40],
+      transition: {
+        duration: 2,
+        ease: easeInOut,
+      },
+    },
+    lightning: {
+      opacity: [1, 1, 0],
     },
   };
 
-  const [clicked, setClicked] = useState(false);
+  const [clicked_K, koreaClicked] = useState(false);
+  const [clicked_Y, yonseiClicked] = useState(false);
 
   return (
     <div className="container">
@@ -23,29 +55,69 @@ export function SoccerKorea({ goNextEvent }) {
         <h1 className="info_title">이길 것 같은 팀을<br/> 선택해주세요</h1>
       </div>
       <div className="character_container">
-        <img src="images/tiger.svg" alt="" className="tiger"/>
-        {/* <img src="images/soccerball.svg" alt="" className="soccerball"/> */}
-        <img src="images/eagle.svg" alt="" className="eagle"/>
+        <motion.img
+          src="images/tiger.svg"
+          alt=""
+          className="tiger"
+          variants={variants}
+          animate={clicked_Y ? "scaled_K" : ""}
+          transition={{ duration: 2 }}
+        />
+        <motion.img
+          src="images/soccerball.svg"
+          alt="" 
+          className="soccerball" 
+          variants={variants}
+          animate={clicked_Y ? "ball" : ""}
+          transition={{ duration: 2 }}  
+        />
+        <motion.img
+          src="images/eagle.svg"
+          alt=""
+          className="eagle"
+          variants={variants}
+          animate={clicked_Y ? "scaled_Y" : ""}
+          transition={{ duration: 2 }}
+        />
       </div>
       <div className="event__btnRow">
         <div className="button_container">
           <motion.button
-            className="bouncing-button"
+            className="rotating-button"
             variants={variants}
             id="korea"
             onClick={() => {
-              setClicked(true);
+              yonseiClicked(true);
             }}
-            animate={clicked ? "click" : ""}
-            transition={{ duration: 0.8, ease: easeInOut }}
+            animate={clicked_K ? "click" : ""}
+            transition={{ duration: 2, ease: easeInOut }}
           >
-            <img src="images/korea_logo.svg" alt="고대" className="korea_logo"/>
+            <motion.img
+              src="images/korea_logo.svg"
+              alt="고대"
+              className="korea_logo"
+              variants={variants}
+              animate={clicked_K ? "korea_logo" : ""}
+              transition={{ duration: 2 }}
+            />
           </motion.button>
         </div>
-        <img className="versus-icon" src="images/lightning.png" alt="아이콘" />
-        <div className="button_container">
-          <button id="yonsei" onClick={goNextEvent}>
-            <img src="images/yonsei_logo.svg" alt="연대" className="yonsei_logo"/>
+        <motion.img
+          className="versus-icon"
+          src="images/lightning.png"
+          variants={variants}
+          animate={clicked_Y ? "lightning" : ""}
+          transition={{ duration: 2 }}
+        />
+        <div className="button_container" onClick={() => { koreaClicked(true) }}>
+          <button id="yonsei">
+            <motion.img
+              className="yonsei_logo"
+              src="images/yonsei_logo.svg"
+              variants={variants}
+              animate={clicked_Y ? "yonsei_logo" : ""}
+              transition={{ duration: 2 }}
+            />
           </button>
         </div>
       </div>
