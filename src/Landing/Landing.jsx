775db67ccf95df235ffdf729de88f203d0865f6e
dennis <dennis2311@daum.net>
2023-08-27 22:30:51 +0900

    랜딩 페이지 디자인 작업

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -6,7 +6,7 @@ import "./landing.css";
  * 결과 페이지에서 카카오톡 공유하기 기능을 사용할 때, 공유하는 URL 에 반영됩니다.
  * @author 현웅
  */
-export function Landing({ goNextPage, setSelectedUniv }) {
+export function Landing({ goNextPage, univ, setSelectedUniv }) {
   function onSelectKorea() {
     setSelectedUniv("KOREA");
     goNextPage();
@@ -18,14 +18,63 @@ export function Landing({ goNextPage, setSelectedUniv }) {
   }
 
   return (
-    <div className="page-container">
-      <span className="landing__title">프로젝트 오대빵</span>
+    <div className="landing-page-container">
+      <div className="title_image_container">
+        <div className="year_image_container">
+          <img
+            src="src/Resource/2023-image.png"
+            alt="2023-image"
+            className="year_image"
+          />
+        </div>
+        {univ === "KOREA" ? (
+          <img
+            className="title_image"
+            src="src/Resource/odebbang-title-korea-image.png"
+            alt="odebbang-title-image"
+          />
+        ) : (
+          <img
+            className="title_image"
+            src="src/Resource/odebbang-title-yonsei-image.png"
+            alt="odebbang-title-image"
+          />
+        )}
+      </div>
 
-      <span className="landing__content">응원할 대학 선택하기</span>
+      <span className="event_description">정기전 승부 예측 이벤트</span>
 
-      <div className="row">
-        <button onClick={onSelectKorea}>고려대</button>
-        <button onClick={onSelectYonsei}>연세대</button>
+      <div className="character_images_container">
+        <img
+          className="character-image tiger-image"
+          src="images/tiger-character.svg"
+          alt="호랑이 캐릭터"
+        ></img>
+        <img
+          className="character-image eagle-image"
+          src="images/eagle-character.svg"
+          alt="독수리 캐릭터"
+        ></img>
+      </div>
+
+      <span className="select_text">응원할 대학을 선택해주세요</span>
+
+      <div className="buttons-container">
+        <div className="button-container korea">
+          <div id="korea" className="univ-button" onClick={onSelectKorea}>
+            <img id="korea-logo" src="images/korea_logo.svg" alt="고대" />
+          </div>
+        </div>
+        <img
+          className="lightning-icon"
+          src="images/lightning.png"
+          alt="아이콘"
+        />
+        <div className="button-container yonsei">
+          <div id="yonsei" className="univ-button" onClick={onSelectYonsei}>
+            <img id="yonsei-logo" src="images/yonsei_logo.svg" alt="연대" />
+          </div>
+        </div>
       </div>
     </div>
   );
