9d6cbd01ad35d93fdbf9ec580a83d5ba0d7a7d19
Jerry <blay210@yonsei.ac.kr>
2023-08-30 04:45:12 +0900

    Basketball Yonsei 최종 수정 (디자인 다른 부분 수정)

--- src/Event/Basketball/Basketball.yonsei.jsx
+++ src/Event/Basketball/Basketball.yonsei.jsx
@@ -3,7 +3,7 @@
  * @author 현웅
  */
 
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import anime from 'animejs';
 import './Basketball.yonsei.css';
 
@@ -17,14 +17,14 @@ export function BasketballYonsei({ goNextEvent }) {
 
 
 
-  const handleYonseiClick = () => {
+  const handleYonseiClick = () => { // Baseball Yonsei 참고
 
     // 빨간색 배경 없애기
     anime({
-      targets: '.page-wrapper',
-      backgroundPosition: '150% 300%',
+      targets: ".page-wrapper",
+      backgroundPosition: "150% 300%",
       duration: 1200,
-      easing: 'easeOutExpo',
+      easing: "easeOutExpo",
       complete: () => {
         setTimeout(() => {
           goNextEvent();
@@ -34,70 +34,67 @@ export function BasketballYonsei({ goNextEvent }) {
 
     // 파란색 그라데이션 배경 추가
     anime({
-      targets: '.page-background',
+      targets: ".page-background",
       opacity: 1,
       duration: 1200,
-      easing: 'easeOutExpo',
+      easing: "easeOutExpo",
     });
 
     //독수리
     anime({
-      targets: '.eagle-image',
-      width: '240px',
-      height: '330px',
-      bottom: '45%',
-      right: '50%',
-      translateX: '50%',
-      translateY: '50%',
+      targets: ".eagle-image",
+      width: "240px",
+      height: "330px",
+      bottom: "45%",
+      right: "50%",
+      translateX: "50%",
+      translateY: "50%",
       duration: 1200,
-      easing: 'easeOutExpo',
+      easing: "easeOutExpo",
     });
 
     //호랑이
     anime({
-      targets: '.tiger-image',
-      width: '0',
-      height: '0',
-      bottom: '45%',
+      targets: ".tiger-image",
+      width: "0",
+      height: "0",
+      bottom: "45%",
       duration: 1200,
-      easing: 'easeOutExpo',
+      easing: "easeOutExpo",
     });
 
     anime({
-      targets: '.ball-image',
-      top: '70%',
-      left: '15%',
+      targets: ".ball-image",
+      top: "80%",
+      left: "25%",
       duration: 1200,
-      scale: 1.2,
-      easing: 'easeOutExpo',
+      easing: "easeOutExpo",
     });
 
     anime({
-      targets: '.prompt-text',
+      targets: ".prompt-text",
       duration: 500,
-      easing: 'easeOutExpo',
-      opacity: 0,
+      easing: "easeOutExpo",
+      fontSize: "45px",
     });
 
+    var firstText = document.querySelector(".prompt-text.first");
+    var secondText = document.querySelector(".prompt-text.second");
+    firstText.innerHTML = "&apos;연세대&apos;";
+    secondText.innerHTML = "승리";
+
     anime({
-      targets: '.result-text',
-      duration: 1300,
-      easing: 'easeOutExpo',
+      targets: ".result-image",
+      duration: 1200,
+      easing: "easeOutExpo",
       opacity: 1,
     });
 
     anime({
-      targets: '.result-image',
+      targets: ".result-image-container",
       duration: 1200,
-      easing: 'easeOutExpo',
-      opacity: 1,
-    });
-
-    anime({
-      targets: '.resultimage-container',
-      duration: 1200,
-      easing: 'easeOutExpo',
-      bottom: '40%',
+      easing: "easeOutExpo",
+      bottom: "40%",
     });
   };
 
@@ -112,12 +109,12 @@ export function BasketballYonsei({ goNextEvent }) {
       <div className="header-container">
         <h5 className="headertext-round">Round 2</h5>
         <h3 className="headertext-event">농구</h3>
-        <div className="resultimage-container">
-          <img className="result-image" src="images/congratulation.svg" alt="승리 이미지"></img>
+        <div className="result-image-container">
+          <img className="result-image" src="images/congratulation.svg" alt="승리 이미지"/>
         </div>
         <div className="prompt-container">
-          <h1 className="prompt-text">이길 것 같은 팀을</h1>
-          <h1 className="prompt-text">선택해주세요</h1>
+          <h1 className="prompt-text first">이길 것 같은 팀을</h1>
+          <h1 className="prompt-text second">선택해주세요</h1>
         </div>
         <div className="result-container">
           <h4 className="result-text">&apos;연세대&apos;</h4>
@@ -132,13 +129,13 @@ export function BasketballYonsei({ goNextEvent }) {
         <div className="buttons-container">
           <div className="button-container korea">
             <div id="btn1" className={`univ-button ${isClicked ? 'flipK' : ''}`} onClick={handleKoreaClick}>
-              <img src="images/korea_logo.svg" alt="고대" />
+              <img id="korea-logo" src="images/korea_logo.svg" alt="고대" />
             </div>
             {isClicked && (<div id="btn2" className={`univ-button ${isClicked ? 'flipY' : ''}`} onClick={handleYonseiClick}><img src="images/yonsei_logo.svg" alt="연대" /></div>)}
           </div>
           <div className="button-container yonsei">
             <div id="yonsei" className="univ-button" onClick={handleYonseiClick}>
-              <img src="images/yonsei_logo.svg" alt="연대" />
+              <img id="yonsei-logo" src="images/yonsei_logo.svg" alt="연대" />
             </div>
           </div>
           <img className="lightning-icon" src="images/lightning.png" alt="아이콘" />
