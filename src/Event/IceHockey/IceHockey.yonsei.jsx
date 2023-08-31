9e7449691c5d2e0b5e22c877f20e922feb79ffe1
dennis <dennis2311@daum.net>
2023-09-01 00:18:00 +0900

    결과 페이지 공유 버튼 태그를 div -> button 으로 변경

--- src/Event/IceHockey/IceHockey.yonsei.jsx
+++ src/Event/IceHockey/IceHockey.yonsei.jsx
@@ -22,9 +22,6 @@ export function IceHockeyYonsei({ goNextEvent }) {
       },
       false
     );
-    return () => {
-      document.removeEventListener("mousemove", () => {});
-    };
   }, []);
 
   const getRandom = (min, max) => {
